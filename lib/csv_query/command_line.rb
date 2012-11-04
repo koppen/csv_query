161a81c1529baca7a7bcaee32d755343bfe45c55
Jakob Skjerning <jakob@mentalized.net>
2012-11-04 22:08:53 +0100

    Improve docs

--- lib/csv_query/command_line.rb
+++ lib/csv_query/command_line.rb
@@ -11,34 +11,40 @@ module CsvQuery
 
       OptionParser.new do |opts|
         opts.banner = "Usage: csvq [options] [CSV file]"
-
+        opts.separator ""
+        opts.separator "Specific options:"
         opts.on(
           "-d",
           "--delimiter DELIMITER",
-          "Sets the DELIMITER used between fields in the CSV data."
+          "Sets the DELIMITER used between fields in the CSV data. Default: #{options[:delimiter].inspect}"
         ) do |d|
           options[:delimiter] = d
         end
         opts.on(
           "-q",
           "--query SQL",
-          "The SQL query to run on the dataset."
+          "The SQL query to run on the dataset. If specified --select and --where will be ignored."
         ) do |q|
           options[:sql_query] = q
         end
         opts.on(
           "-s",
           "--select SQL",
-          "The SQL statement to select what fields to return."
+          "The SQL statement to select what fields to return. Unused if --query is given. Default: #{options[:select].inspect}."
         ) do |s|
           options[:select] = s
         end
         opts.on(
           "-w",
           "--where SQL",
-          "The SQL conditions to use for quering the dataset."
+          "The SQL conditions to use for quering the dataset. Unused if --query is given."
         ) do |w|
           options[:where] = w
+        end
+
+        opts.on_tail("-h", "--help", "Show this message.") do
+          puts opts
+          exit
         end
       end.parse!
 
